448de1bbd8ca28f8ed929b5e7e64d7ca6f8f82ba
Tenvy <87992922+Tenvy@users.noreply.github.com>
2023-05-23 23:45:23 +0800

    add: delete method

--- script.ts
+++ script.ts
@@ -1,4 +1,5 @@
 interface Post {
+  id: number;
   profile_image: string;
   username: string;
   post_image: string;
@@ -8,30 +9,35 @@ interface Post {
 class MediaPost {
   private posts: Post[] = [
 		{
+      id: 5,
 			profile_image: "./assets/anime.png",
 			username: "sisca024",
 			post_image: "./assets/wallpaper.png",
 			description: "Absolutely breathtaking! 😍🌅 Sharing a magical moment with my love on the edge of a cliff, overlooking the mesmerizing sunset. (•‿•)💑 The golden hues of the sky blend perfectly with the warmth of our embrace, creating a picture-perfect memory. (◑‿◐)✨",
 		},	
 		{
+      id: 4,
 			profile_image: "./assets/raya.png",
 			username: "raya_re",
 			post_image: "./assets/spaghetti.webp",
 			description: "If you had one shot or one opportunity to seize everything you ever wanted, one moment would you captured it? or just let it slip",
 		},
 		{
+      id: 3,
 			profile_image: "./assets/abduh.jpg",
-			username: "abduh23",
+			username: " abduh23",
 			post_image: "./assets/berserk.jpg",
 			description: "Jalani hari dengan sepenuh hati, Jangan sesali apa yang sudah terjadi.",
 		},
 		{
+      id: 2,
 			profile_image: "./assets/odori.jpeg",
 			username: "tenvy.official",
 			post_image: "./assets/profile.webp",
 			description: "Dengan kekasih tercinta!, aku mencintainya lebih dari dirinya mencintaiku <3. namanya selalu ada di hatiku💖, mengingatnya selalu membuat",
 		},
 		{
+      id: 1,
 			profile_image: "./assets/profile.webp",
 			username: "ai_hoshino",
 			post_image: "./assets/post.jpg",
@@ -39,47 +45,68 @@ class MediaPost {
 		},
   ];
 
-  public addPost(newPost: Post): void {
-    this.posts.push({ ...newPost });
+  private postIdCounter: number = 5;
+
+  public addPost(newPost: Omit<Post , "id">): void {
+    const post: Post = {
+      id: this.postIdCounter++,
+      ...newPost,
+    };
+    this.posts.push(post);
+  }
+
+  public deletePost(selectedPost: number):void{
+    const index = this.posts.findIndex(posts => posts.id === selectedPost);
+    if ( index !== -1 ) {
+        this.posts.splice(index, 1);
+    }
   }
 
   public getPosts(): Post[] {
-    return this.posts.reverse();
+    return this.posts.slice().reverse();
   }
 
-  
 }
-
 
 const media = new MediaPost();
 
 const modal = document.querySelector(".modal-container") as HTMLElement;
 const postContainer = document.querySelector(".post") as HTMLElement;
 
-const loadPosts = () => {
-  const html = media.getPosts().map((post) => 
-		  	`
-          <div class="post-list">
-            <div class="head-post">
-              <div>
-                <img class="image-profile" src="${post.profile_image}" alt="profile-picture">
-              </div>
-              <h2>${post.username}</h2>
-            </div>
-            <div class="image-post-container">
-              <img class="image-post" src="${post.post_image}" alt="image-post">
-            </div>
-            <div class="description-container">
-              <div class="description">
-                <h2>from ${post.username}</h2>
-                <p>${post.description}</p>
-              </div>
-            </div>
+const loadPosts = () : void => {
+  const html = media.getPosts().map((post) => `
+      <div class="post-list">
+        <div class="head-post">
+          <div>
+            <img class="image-profile" src="${post.profile_image}" alt="profile-picture">
+            <h2>${post.username}</h2>
           </div>
-        `
-    ).join("");
+          <button onclick="deletePost(event)" data-id="${post.id}" class="delete-button">Delete</button>
+        </div>
+        <div class="image-post-container">
+          <img class="image-post" src="${post.post_image}" alt="image-post">
+        </div>
+        <div class="description-container">
+          <div class="description">
+            <h2>from ${post.username}</h2>
+            <p>${post.description}</p>
+          </div>
+        </div>
+      </div>
+    `)
+    .join("");
 
   postContainer.innerHTML = html;
+};
+
+const deletePost = (event: Event) => {
+  const button = event.target as HTMLButtonElement;
+  const id = button.getAttribute("data-id");
+  if (id) {
+    const postId = parseInt(id);
+    media.deletePost(postId);
+    loadPosts();
+  }
 };
 
 const form = document.querySelector("#post_form") as HTMLFormElement;
@@ -87,21 +114,14 @@ const form = document.querySelector("#post_form") as HTMLFormElement;
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-	const objInput = {
-		profileInput : (document.querySelector("#profile_input") as HTMLInputElement).files,
-		usernameInput : (document.querySelector("#username_input") as HTMLInputElement).value,
-		postInput : (document.querySelector("#post_input") as HTMLInputElement).files,
-		descriptionInput : (document.querySelector("#description_input") as HTMLInputElement).value,
-	}
+  const profileInput = (document.querySelector("#profile_input") as HTMLInputElement).files;
+  const usernameInput = (document.querySelector("#username_input") as HTMLInputElement).value;
+  const postInput = (document.querySelector("#post_input") as HTMLInputElement).files;
+  const descriptionInput = (document.querySelector("#description_input") as HTMLInputElement).value;
 
-  if (
-    objInput.profileInput &&
-    objInput.usernameInput &&
-    objInput.postInput &&
-    objInput.descriptionInput
-  ) {
-    const profileImageFile = objInput.profileInput[0];
-    const postImageFile = objInput.postInput[0];
+  if (profileInput && usernameInput && postInput && descriptionInput) {
+    const profileImageFile = profileInput[0];
+    const postImageFile = postInput[0];
 
     const profileImageReader = new FileReader();
     const postImageReader = new FileReader();
@@ -113,9 +133,9 @@ form.addEventListener("submit", (e) => {
       modal.style.display = "none";
       media.addPost({
         profile_image: profileImageUrl,
-        username: objInput.usernameInput,
+        username: usernameInput,
         post_image: postImageUrl,
-        description: objInput.descriptionInput,
+        description: descriptionInput,
       });
       loadPosts();
       form.reset();
@@ -128,12 +148,13 @@ form.addEventListener("submit", (e) => {
 
   }
 });
+
 // Create post button toggle
-const modalHandler = () => {
+const modalHandler = (): void => {
   modal.style.display = "flex";
 };
 
-const postButtonClickHandler = () => {
+const postButtonClickHandler = (): void => {
   modalHandler();
 };
 
@@ -145,11 +166,11 @@ postButtons.forEach((button) => {
 
 // dark theme toggle
 
-const themeHandler = () => {
+const themeHandler = (): void => {
   document.body.classList.toggle("dark-theme")
 }
 
-const darkToggleHandler = () => {
+const darkToggleHandler = (): void => {
   themeHandler()
 }
 
--- src/script.js
+++ src/script.js
@@ -3,42 +3,55 @@ class MediaPost {
     constructor() {
         this.posts = [
             {
+                id: 5,
                 profile_image: "./assets/anime.png",
                 username: "sisca024",
                 post_image: "./assets/wallpaper.png",
                 description: "Absolutely breathtaking! 😍🌅 Sharing a magical moment with my love on the edge of a cliff, overlooking the mesmerizing sunset. (•‿•)💑 The golden hues of the sky blend perfectly with the warmth of our embrace, creating a picture-perfect memory. (◑‿◐)✨",
             },
             {
+                id: 4,
                 profile_image: "./assets/raya.png",
                 username: "raya_re",
                 post_image: "./assets/spaghetti.webp",
                 description: "If you had one shot or one opportunity to seize everything you ever wanted, one moment would you captured it? or just let it slip",
             },
             {
+                id: 3,
                 profile_image: "./assets/abduh.jpg",
-                username: "abduh23",
+                username: " abduh23",
                 post_image: "./assets/berserk.jpg",
                 description: "Jalani hari dengan sepenuh hati, Jangan sesali apa yang sudah terjadi.",
             },
             {
+                id: 2,
                 profile_image: "./assets/odori.jpeg",
                 username: "tenvy.official",
                 post_image: "./assets/profile.webp",
                 description: "Dengan kekasih tercinta!, aku mencintainya lebih dari dirinya mencintaiku <3. namanya selalu ada di hatiku💖, mengingatnya selalu membuat",
             },
             {
+                id: 1,
                 profile_image: "./assets/profile.webp",
                 username: "ai_hoshino",
                 post_image: "./assets/post.jpg",
                 description: "OMG! 🥞🌸 Just had the most amazing pancakes ever! (•‿•)⁠ These fluffy delights are pure happiness on a plate! (◑‿◐)✨ Topped with fresh fruits, drizzled with maple syrup, and dusted with powdered sugar, they were a taste sensation! (-‿◦☀)✨ I can't even describe how heavenly they were! ಥ‿ಥ💖 Who else loves pancakes like this? (づ ◕‿◕ )づ🌈",
             },
         ];
+        this.postIdCounter = 5;
     }
     addPost(newPost) {
-        this.posts.push(Object.assign({}, newPost));
+        const post = Object.assign({ id: this.postIdCounter++ }, newPost);
+        this.posts.push(post);
+    }
+    deletePost(selectedPost) {
+        const index = this.posts.findIndex(posts => posts.id === selectedPost);
+        if (index !== -1) {
+            this.posts.splice(index, 1);
+        }
     }
     getPosts() {
-        return this.posts.reverse();
+        return this.posts.slice().reverse();
     }
 }
 const media = new MediaPost();
@@ -46,41 +59,47 @@ const modal = document.querySelector(".modal-container");
 const postContainer = document.querySelector(".post");
 const loadPosts = () => {
     const html = media.getPosts().map((post) => `
-          <div class="post-list">
-            <div class="head-post">
-              <div>
-                <img class="image-profile" src="${post.profile_image}" alt="profile-picture">
-              </div>
-              <h2>${post.username}</h2>
-            </div>
-            <div class="image-post-container">
-              <img class="image-post" src="${post.post_image}" alt="image-post">
-            </div>
-            <div class="description-container">
-              <div class="description">
-                <h2>from ${post.username}</h2>
-                <p>${post.description}</p>
-              </div>
-            </div>
+      <div class="post-list">
+        <div class="head-post">
+          <div>
+            <img class="image-profile" src="${post.profile_image}" alt="profile-picture">
+            <h2>${post.username}</h2>
           </div>
-        `).join("");
+          <button onclick="deletePost(event)" data-id="${post.id}" class="delete-button">Delete</button>
+        </div>
+        <div class="image-post-container">
+          <img class="image-post" src="${post.post_image}" alt="image-post">
+        </div>
+        <div class="description-container">
+          <div class="description">
+            <h2>from ${post.username}</h2>
+            <p>${post.description}</p>
+          </div>
+        </div>
+      </div>
+    `)
+        .join("");
     postContainer.innerHTML = html;
+};
+const deletePost = (event) => {
+    const button = event.target;
+    const id = button.getAttribute("data-id");
+    if (id) {
+        const postId = parseInt(id);
+        media.deletePost(postId);
+        loadPosts();
+    }
 };
 const form = document.querySelector("#post_form");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const objInput = {
-        profileInput: document.querySelector("#profile_input").files,
-        usernameInput: document.querySelector("#username_input").value,
-        postInput: document.querySelector("#post_input").files,
-        descriptionInput: document.querySelector("#description_input").value,
-    };
-    if (objInput.profileInput &&
-        objInput.usernameInput &&
-        objInput.postInput &&
-        objInput.descriptionInput) {
-        const profileImageFile = objInput.profileInput[0];
-        const postImageFile = objInput.postInput[0];
+    const profileInput = document.querySelector("#profile_input").files;
+    const usernameInput = document.querySelector("#username_input").value;
+    const postInput = document.querySelector("#post_input").files;
+    const descriptionInput = document.querySelector("#description_input").value;
+    if (profileInput && usernameInput && postInput && descriptionInput) {
+        const profileImageFile = profileInput[0];
+        const postImageFile = postInput[0];
         const profileImageReader = new FileReader();
         const postImageReader = new FileReader();
         profileImageReader.onload = () => {
@@ -89,9 +108,9 @@ form.addEventListener("submit", (e) => {
             modal.style.display = "none";
             media.addPost({
                 profile_image: profileImageUrl,
-                username: objInput.usernameInput,
+                username: usernameInput,
                 post_image: postImageUrl,
-                description: objInput.descriptionInput,
+                description: descriptionInput,
             });
             loadPosts();
             form.reset();
